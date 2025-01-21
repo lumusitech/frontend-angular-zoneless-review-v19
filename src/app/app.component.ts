7fa1b101a2946d728f5206f24d671fb65dcd7d45
Luciano Figueroa <lumusitech@gmail.com>
2025-01-21 02:26:47 +0000

    refactor: organize code into components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { CharacterGridComponent } from './character/character-grid/character-grid.component';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CharacterGridComponent],
+  imports: [CharacterGridComponent, FormComponent],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
